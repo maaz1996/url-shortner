1413a2034f6719acdeed5bdb4fbf925cfade19e9
maaz1996 <maaz.abdullah111@gmail.com>
2022-10-16 18:53:08 +0530

    get url without auth

--- routes/urlroute.js
+++ routes/urlroute.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const UrlController = require("../controller/urlcontroller");
 const middleware = require("../config/auth");
 router.post("/url", middleware.auth, UrlController.url);
-router.get("/:code", middleware.auth, UrlController.geturl);
+router.get("/:code", UrlController.geturl);
 module.exports = router;
